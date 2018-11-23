70c692de5e414cd25a66cd0f79db985f66b843fb
Sourav Khoso <souravkhoso1@gmail.com>
2018-11-23 18:00:11 +0530

    base version

---
+++ app.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+	//console.log(req.url);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	if(req.url==1){
+		var content = fs.readFileSync('Bhubaneswar.txt', 'utf8');
+	}
+	res.end(content);
+});
+
+//var topic = 'Bhubaneswar';
+//var content = fs.readFileSync(topic+'.txt', 'utf8');
+
+//console.log(content);
+
+
+server.listen(3000, '127.0.0.1');
+console.log('server started');
