33c61331d941d2c09f9541e8ea6d9481704b3706
Sourav Khoso <souravkhoso1@gmail.com>
2018-11-23 20:25:13 +0530

    added list_all method

--- app.js
+++ app.js
@@ -3,11 +3,39 @@ var express = require('express')
 var app = express()
 const port = 3000;
 
-app.get('/gk/:topic', function (req, res) {
-	fs.readFile(req.params.topic+'.txt', 'utf8', function(err, contents) {
-    	res.send(contents);
-	})
+l = ""
+c = 0
+ans = "[";
+
+fs.readdirSync('./').forEach(file => {
+    if(file.indexOf('txt') >=0 ){
+        c++;
+        ans += "'" + file.substr(0, file.length-4) + "',";
+    }
 })
+
+ans = ans.slice(0, ans.length-1) +  "]"
+l = "{'count': " + c + ", 'topics': " + ans + "}"
+
+// fs.readdir('./', (err, files) => {
+//   files.forEach(file => {
+//     l = l + file
+//   });
+// })
+
+console.log(l)
+
+app.get('/gk/:topic', function (req, res) {
+    if(req.params.topic === 'list_all'){
+        res.send(l);
+    } else {
+	    fs.readFile(req.params.topic+'.txt', 'utf8', function(err, contents) {
+    	   res.send(contents);
+	    })
+    }
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
