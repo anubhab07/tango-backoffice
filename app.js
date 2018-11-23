ef14d2b90b83ee9a2b1b2492175f8dafdde94803
Sourav Khoso <souravkhoso1@gmail.com>
2018-11-23 19:04:52 +0530

    made topics dynamic

--- app.js
+++ app.js
@@ -2,22 +2,15 @@ var fs = require('fs');
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-	//console.log(req.url);
-	var content = "";
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	if(req.url==='/1'){
-		fs.readFile('Bhubaneswar.txt', 'utf8', function(err, data){
-			content = data;
-		});
+	
+	var topic = req.url.replace("/gk/", "");
+	if(topic.length>0){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		fs.createReadStream(__dirname + '/'+ topic + '.txt').pipe(res);
+	} else {
+		res.writeHead(404);
 	}
-	res.end(content);
 });
-
-//var topic = 'Bhubaneswar';
-//var content = fs.readFileSync(topic+'.txt', 'utf8');
-
-//console.log(content);
-
 
 server.listen(3000, '0.0.0.0');
 console.log('server started');
